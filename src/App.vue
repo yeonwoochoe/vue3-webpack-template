baa8a94d4d6e0ab2c1089ce3296ca30c117e527f
yeonwoo <chiyy6162@gmail.com>
2022-04-19 21:18:12 +0900

    vue3-webpack-template-eslint

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">4보다 큽니다!</div>
+  <ul>
+    <Fruit v-for="fruit in fruits" :key="fruit" :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from "~/components/Fruit";
+
+export default {
+  components: {
+    Fruit,
+    //콜론 생략가능
+  },
+
+  data() {
+    return {
+      count: 0,
+      fruits: ["apple", "banana", "cherry"],
+    };
+  },
+  methods: {
+    increase() {
+      this.count += 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h1 {
+  font-size: 50px;
+  color: blue;
+}
+ul {
+  li {
+    font-size: 30px;
+  }
+}
+</style>
